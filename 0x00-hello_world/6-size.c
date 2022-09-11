01ce941869113d8c4ac97bdec183f5189a70cc27
UkaeremOnyinyechi <erem.onyinyechi@gmail.com>
2022-09-11 09:47:09 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 
 /**
   * main - print the string in the put function
- i *
+  *
   * Description: using the main function
   * this program prints "Programming is like building a multilingual puzzle
   * Return: 0
