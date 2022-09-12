12341523d8f0472e6811b89020996e82f353b99d
UkaeremOnyinyechi <erem.onyinyechi@gmail.com>
2022-09-12 09:55:29 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - print size of data
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
