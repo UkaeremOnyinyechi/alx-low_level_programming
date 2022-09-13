1c0dbfb57a7f81c150eae31744efadad1ab89d5f
UkaeremOnyinyechi <erem.onyinyechi@gmail.com>
2022-09-12 20:56:08 -0700

    task0 and 2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,47 +7,33 @@
 
 
 /**
-
  * main - print if the number is positive, negative and zero
- *
+ * 
  * Description: Using the main function
  * this program prints "Programming is positive, negative and zero"
  * Return: 0
  */
 
 int main(void)
-
 {
-
 int n;
 
 srand(time(0));
+
 n = rand() - RAND_MAX / 2;
 
 if (n > 0)
-
 {
-
-	printf("%i is positive\n", n);
-
+        printf("%i is positive\n", n);
 }
 
 if (n == 0)
-
 {
-
 	printf("%i is zero\n", n);
-
 }
-
 if (n < 0)
-
 {
-
-	printf("%i is negative\n", n);
-
+	printf("%i is negative\n", n)
 }
-
 return (0);
-
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+#include <time.h>
+
+
+
+/**
+ * main - prints the program in lowercase followed
+ * by a new line.
+ *
+ * Description: a program that prints the alphabet in
+ * lowercase, followed by a new line.
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+
+   putchar('\n');
+
+   return (0);
+}
