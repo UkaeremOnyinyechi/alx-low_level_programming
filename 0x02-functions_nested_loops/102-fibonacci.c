179d82c6bf53b051436bfeae6938b68882abd438
UkaeremOnyinyechi <erem.onyinyechi@gmail.com>
2022-09-14 21:59:28 -0700

    y

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,15 +3,10 @@
 
 
 /**
-
  * main - main block
-
  * Description: Print the first 50 fibonacci numbers, starting with 1 and 2.
-
  * Numbers must be coma and space separated.
-
  * Return: 0
-
  */
 
 
