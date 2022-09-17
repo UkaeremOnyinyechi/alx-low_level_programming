cd9ddb69cdc2047ec63b313a33b5ea2cc53e8a5f
UkaeremOnyinyechi <erem.onyinyechi@gmail.com>
2022-09-16 20:53:16 -0700

    task5

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,46 +1,32 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: random integer
- *Return: void
- **/
+ *print_square - prints squares
+ *@size: parameter
+ *Return: returns nothing
+ */
 
-void print_number(int n)
+void print_square(int size)
 {
-	char ld, cr;
-	int r;
-	int ct = 0;
+		int inc1, inc2;
 
-	if (n < 0)
-	{
-		_putchar ('-');
-		ld = (char)('0' - (n % 10));
-		n = n / -10;
-	}
-	else
-	{
-		ld = (char)((n % 10) + '0');
-		n = n / 10;
-	}
-	r = 0;
-	while (n > 0)
-	{
-		r = r * 10 + (n % 10);
-		n = n / 10;
-		ct++;
-	}
-	while (r > 0)
-	{
-		cr = (char)((r % 10) + '0');
-		_putchar(cr);
-		r = r / 10;
-		ct--;
-	}
-	while (ct != 0)
-	{
-		_putchar('0');
-		ct--;
-	}
-	_putchar(ld);
+		if (size > 0)
+		{
+			for (inc1 = 0; inc1 < size; inc1++)
+			{
+				for (inc2 = 0; inc2 < (size - 1); inc2++)
+				{
+					putchar('#');
+				}
+				putchar('#');
+				putchar('\n');
+			}
+		}
+		else
+		{
+			putchar('\n');
+		}
 }
+
+
